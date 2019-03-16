620d63574016882919a6e2ac9fe4b5f546f24bac
Melissa Murphy <31637708+thinkingmonkey00@users.noreply.github.com>
2019-03-15 18:31:37 -0700

    client changes

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+import jQuery from 'jquery';
 import $ from 'jquery';
 import popper from 'popper.js';
 import bootstrap from 'bootstrap';
+
+if ( module.hot ) {
+    module.hot.accept(function () {
+      window.location.reload();
+    });
+  }
+
+  jQuery(function ($) {
+    $('#menu > ul.navbar-nav li').click(function(e) {
+        $('.navbar-nav li.active').removeClass('active');
+        $(this).addClass('active');;
+        e.preventDefault();
+    });
+});
