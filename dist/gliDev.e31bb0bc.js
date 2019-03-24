1302683afd592d965b0e7492570ca9bec9eb11e4
melissa-murphy <melissamurphy.ca@gmail.com>
2019-03-23 21:03:15 -0700

    updated about with image/styling

--- dist/gliDev.e31bb0bc.js
+++ dist/gliDev.e31bb0bc.js
@@ -17751,7 +17751,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58016" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59320" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
