ab0e91c0893646328a8e0edd9023f45550dcf293
melissa-murphy <melissamurphy.ca@gmail.com>
2019-07-25 23:24:39 -0700

    store, initial views, connected router

---
+++ gli/src/containers/home.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { push } from 'connected-react-router';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+const Home = props => (
+  <div>
+    <h1>Home</h1>
+    <p>welcome home</p>
+    <button onClick={() => props.changePage()}>About</button>
+  </div>
+);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      changePage: () => push('/about-us')
+    },
+    dispatch
+  );
+
+export default connect(
+  null,
+  mapDispatchToProps(Home)
+);
